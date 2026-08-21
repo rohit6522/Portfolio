344bcf954be66e1cc3dad0d3345a5a0bb857c86b
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-08-21 14:36:56 +0530

    Resume-Uploaded

--- src/data/content.js
+++ src/data/content.js
@@ -16,7 +16,7 @@ export const profile = {
   location: 'Patna, Bihar',
   email: 'rohitrajyadav6522@gmail.com',
 
-  resumeUrl: '/resume.pdf',
+  resumeUrl: '/resumePortfolio.pdf',
   calendlyUrl: 'https://calendly.com/rohitrajyadav6522/30min',
   githubUsername: 'rohit6522',
   leetcodeUsername: 'rohit6522',
