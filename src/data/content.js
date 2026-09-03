d7a3dbbc5fca74daf4fa20e519ef5948528b8142
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-09-03 12:03:46 +0530

    Small Issued solved

--- src/data/content.js
+++ src/data/content.js
@@ -13,7 +13,7 @@ export const profile = {
   location: 'Patna, Bihar',
   email: 'rohitrajyadav6522@gmail.com',
 
-  resumeUrl: '/Resumeportfolio.pdf',
+  resumeUrl: '/resume.pdf',
   calendlyUrl: 'https://calendly.com/rohitrajyadav6522/30min',
   githubUsername: 'rohit6522',
   leetcodeUsername: 'rohit6522',
