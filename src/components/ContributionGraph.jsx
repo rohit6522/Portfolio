6d4dd2dc106662c3d92c2d9f47d1f3658ceff26c
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-09-05 17:49:11 +0530

    Add real GitHub and LeetCode logo icons to contribution graph toggle buttons

--- src/components/ContributionGraph.jsx
+++ src/components/ContributionGraph.jsx
@@ -3,6 +3,29 @@ import { profile } from '../data/content'
 import IconBadge, { CodeIcon } from './IconBadge'
 import MagneticText from './MagneticText'
 
+function GithubLogoIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+      <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.57.1.78-.25.78-.55v-2.1c-3.2.7-3.87-1.36-3.87-1.36-.53-1.34-1.29-1.7-1.29-1.7-1.05-.72.08-.7.08-.7 1.17.08 1.78 1.2 1.78 1.2 1.03 1.77 2.71 1.26 3.37.96.1-.75.4-1.26.73-1.55-2.55-.29-5.24-1.28-5.24-5.7 0-1.26.45-2.29 1.19-3.09-.12-.29-.52-1.46.11-3.05 0 0 .97-.31 3.18 1.18a11.05 11.05 0 0 1 5.79 0c2.2-1.49 3.17-1.18 3.17-1.18.64 1.59.24 2.76.12 3.05.74.8 1.19 1.83 1.19 3.09 0 4.43-2.7 5.4-5.27 5.69.42.36.78 1.07.78 2.16v3.2c0 .3.21.66.79.55A10.52 10.52 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z" />
+    </svg>
+  )
+}
+
+function LeetCodeLogoIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="18" height="18" fill="none">
+      <path
+        d="M13.48 2.3 6.1 9.68a3.5 3.5 0 0 0 0 4.95l4.66 4.66a3.5 3.5 0 0 0 4.95 0l2.2-2.2a1.1 1.1 0 0 0-1.56-1.56l-2.2 2.2a1.3 1.3 0 0 1-1.83 0l-4.66-4.66a1.3 1.3 0 0 1 0-1.83l7.38-7.38A1.1 1.1 0 0 0 13.48 2.3Z"
+        fill="#FFA116"
+      />
+      <path
+        d="M9.5 12.5h9a1.1 1.1 0 0 0 0-2.2h-9a1.1 1.1 0 0 0 0 2.2Z"
+        fill="#000000"
+      />
+    </svg>
+  )
+}
+
 
 const currentYear = new Date().getFullYear()
 const years = [currentYear, currentYear - 1, currentYear - 2]
@@ -195,18 +218,18 @@ export default function ContributionGraph() {
 
         </div>
 
-        <div className="contrib-toggle-row">
+               <div className="contrib-toggle-row">
           <button
             className={`contrib-toggle-btn ${platform === 'github' ? 'active' : ''}`}
             onClick={() => setPlatform('github')}
           >
-            GitHub
+            <GithubLogoIcon /> GitHub
           </button>
           <button
             className={`contrib-toggle-btn ${platform === 'leetcode' ? 'active' : ''}`}
             onClick={() => setPlatform('leetcode')}
           >
-            LeetCode
+            <LeetCodeLogoIcon /> LeetCode
           </button>
         </div>
 
